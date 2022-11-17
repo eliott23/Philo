75974e55a01a095df8d42581c0df67e55a481d51
Abderrahmane Ababachir <aababach@e2r5p5.1337.ma>
2022-11-17 20:58:08 +0100

    school

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <pthread.h>
+#include <sys/time.h>
+
+void *get_timestamp()
+{
+	struct timeval lol;
+
+	gettimeofday(&lol,NULL);
+	printf("this is the size %ld\n",sizeof(long long unsigned));
+	printf("time = %lld\n",(long long unsigned)lol.tv_sec);
+	printf("time = %lld\n",(long long unsigned)lol.tv_usec);
+	return(0);
+}
 
 void	*test()
 {
@@ -11,7 +23,7 @@ void	*test()
 int	main(int ac, char **av)
 {
 	pthread_t t;
-	
-	pthread_create(&t,NULL, &test, NULL);
+
+	pthread_create(&t,NULL, &get_timestamp, NULL);
 	pthread_join(t,NULL);
 }
