47bc1966b107c85870de861969ce6a830f9d0577
Abderrahmane Ababachir <aababach@e2r9p4.1337.ma>
2022-11-19 20:21:25 +0100

    school

--- main.c
+++ main.c
@@ -1,5 +1,13 @@
 #include "philo.h"
 
+void	test(int tv_start)
+{
+	struct timeval t;
+
+	gettimeofday(&t,NULL);
+	printf("%d ms since the start of the program\n",t.tv_usec - tv_start);
+}
+
 void *get_timestamp(void *i)
 {
 	printf("Hi am philosopher number %d\n", (*(int *)i) + 1);
@@ -12,11 +20,12 @@ void *get_timestamp(void *i)
 int	main(int ac, char **av)
 {
 	struct timeval start;
+	gettimeofday(&start,NULL);
 	int	i = 0;
 	int	*temp;
 	int	n_philo = ft_atoi(av[1]);
 	pthread_t *t = malloc(sizeof(pthread_t) * n_philo);
-	gettimeofday(&start,NULL);
+	test(start.tv_usec);
 	while (i < n_philo)
 	{
 		temp = malloc(sizeof(int));
