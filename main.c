2b916df036788be4c49cc862a41bcc45b98c5fb2
Abderrahmane Ababachir <aababach@e2r9p4.1337.ma>
2022-11-19 21:42:07 +0100

    school

--- main.c
+++ main.c
@@ -5,7 +5,8 @@ void	test(int tv_start)
 	struct timeval t;
 
 	gettimeofday(&t,NULL);
-	printf("%ld ms since the start of the program\n",t.tv_sec - tv_start);
+	printf("end time = %ld,%d\n",t.tv_sec, t.tv_usec);
+	printf("%d ms since the start of the program\n",(t.tv_usec - tv_start) / 1000);
 }
 
 void *get_timestamp(void *i)
@@ -21,24 +22,26 @@ int	main(int ac, char **av)
 {
 	struct timeval start;
 	gettimeofday(&start,NULL);
+	printf("start time = %ld,%d\n",start.tv_sec, start.tv_usec);
 	int	i = 0;
 	int	*temp;
-	int	n_philo = ft_atoi(av[1]);
-	pthread_t *t = malloc(sizeof(pthread_t) * n_philo);
-	usleep(100000);
-	test(start.tv_sec);
-	while (i < n_philo)
-	{
-		temp = malloc(sizeof(int));
-		*temp = i;
-		pthread_create(&t[i],NULL, &get_timestamp, temp);
-		i++;
-	}
-	i = 0;
-	while (i < n_philo)
-	{
-		pthread_join(t[i],NULL);
-		i++;
-	}
-	printf("start time = %ld,%d\n",start.tv_sec, start.tv_usec / 1000);
+	usleep(900);
+	// if (ac < 5)
+	// 	return (0);
+	// int	n_philo = ft_atoi(av[1]);
+	// pthread_t *t = malloc(sizeof(pthread_t) * n_philo);
+	// while (i < n_philo)
+	// {
+	// 	temp = malloc(sizeof(int));
+	// 	*temp = i;
+	// 	pthread_create(&t[i],NULL, &get_timestamp, temp);
+	// 	i++;
+	// }
+	// i = 0;
+	// while (i < n_philo)
+	// {
+	// 	pthread_join(t[i],NULL);
+	// 	i++;
+	// }
+	test(start.tv_usec);
 }
