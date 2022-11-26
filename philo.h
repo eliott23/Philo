7d6e59be822f7adf9f418426bcab331d288a5e9b
Abderrahmane Ababachir <aababach@e1r5p5.1337.ma>
2022-11-26 18:00:16 +0100

    norming

--- philo.h
+++ philo.h
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aababach <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/26 17:59:51 by aababach          #+#    #+#             */
+/*   Updated: 2022/11/26 18:00:10 by aababach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PHILO_H
 # define PHILO_H
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <pthread.h>
-#include <sys/time.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <pthread.h>
+# include <sys/time.h>
 
 typedef struct var{
 	int				*d_flag;
@@ -23,7 +35,7 @@ typedef struct var{
 	int				m_eat;
 }t_inf;
 
-long long int   ft_atoi(const char *str);
+long long int	ft_atoi(const char *str);
 void			ft_init(t_inf *temp, char **av, int ac);
 long long		get_timestamp(struct timeval start);
 void			ft_usleep(long long v, t_inf inf);
