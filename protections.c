a7511da1bc0945c81e21e6cf17ab0447ab29c04c
Abderrahmane Ababachir <aababach@e1r4p5.1337.ma>
2022-11-30 12:38:13 +0100

    norming

--- protections.c
+++ protections.c
@@ -6,7 +6,7 @@
 /*   By: aababach <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 20:51:25 by aababach          #+#    #+#             */
-/*   Updated: 2022/11/29 20:55:54 by aababach         ###   ########.fr       */
+/*   Updated: 2022/11/30 12:37:49 by aababach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	ft_p(t_inf *inf, int *i)
 	*i = 0;
 	if (inf->n_philo <= 0 || inf->t_die <= 0 || \
 			inf->t_eat <= 0 || inf->t_sleep <= 0)
+		return (0);
+	if (!(inf->n_eat) && !(inf->mutex) && !(inf->death_mutex) && !(inf->last_meal) && !(inf->d_flag))
 		return (0);
 	return (1);
 }
