b2f6f6edb94ac9d4d56958540d18ede91bd458ec
Abderrahmane Ababachir <aababach@e1r5p8.1337.ma>
2022-12-02 16:05:54 +0100

    norming

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: aababach <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/26 17:41:30 by aababach          #+#    #+#             */
-/*   Updated: 2022/12/02 16:02:19 by aababach         ###   ########.fr       */
+/*   Updated: 2022/12/02 16:05:03 by aababach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -108,13 +108,6 @@ int	main(int ac, char **av)
 		*inf = temp;
 		inf->i = i + 1;
 		pthread_create(&t[i], NULL, &rout, inf);
-		i++;
-	}
-	i = 0;
-	while (i < temp.n_philo)
-	{
-		pthread_mutex_lock(&(temp.death_mutex[i]));
-		pthread_mutex_unlock(&(temp.death_mutex[i]));
 		i++;
 	}
 	if (!(m_helper(temp, 0, count)))
